262c5ee817f4a283290a095a62a326ef742685f3
twkkwn <twkkwn@gmail.com>
2025-08-18 10:54:06 +0900

    [Modify] 문항 변경
    출제 문제 변경

--- app/app/data/questions.ts
+++ app/app/data/questions.ts
@@ -9,7 +9,7 @@ export type Question = {
 export const ALL_QUESTIONS: Question[] = [
   { id: 1, image: '/assets/quiz-1.png', correctIndex: 4, answerImageO: '/assets/quiz-1-o.png', answerImageX: '/assets/quiz-1-x.png' },
   { id: 2, image: '/assets/quiz-2.png', correctIndex: 3, answerImageO: '/assets/quiz-2-o.png', answerImageX: '/assets/quiz-2-x.png' },
-  { id: 3, image: '/assets/quiz-3.png', correctIndex: 3, answerImageO: '/assets/quiz-3-o.png', answerImageX: '/assets/quiz-3-x.png' },
+  { id: 3, image: '/assets/quiz-3.png', correctIndex: 1, answerImageO: '/assets/quiz-3-o.png', answerImageX: '/assets/quiz-3-x.png' },
   { id: 4, image: '/assets/quiz-4.png', correctIndex: 3, answerImageO: '/assets/quiz-4-o.png', answerImageX: '/assets/quiz-4-x.png' },
   { id: 5, image: '/assets/quiz-5.png', correctIndex: 2, answerImageO: '/assets/quiz-5-o.png', answerImageX: '/assets/quiz-5-x.png' },
 ]
