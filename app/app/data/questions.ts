f5a6bd0815e2b622bb1e8f00382e36e36a41e8c8
twkkwn <twkkwn@gmail.com>
2025-11-10 00:47:16 +0900

    문항수 추가

--- app/app/data/questions.ts
+++ app/app/data/questions.ts
@@ -1,7 +1,7 @@
 export type Question = {
-  id: 1|2|3|4|5
+  id: 1|2|3|4|5|6
   image: string
-  correctIndex: 1|2|3|4|5
+  correctIndex: 1|2|3|4|5|6
   answerImageO: string
   answerImageX: string
 }
@@ -12,4 +12,5 @@ export const ALL_QUESTIONS: Question[] = [
   { id: 3, image: '/assets/quiz-3.png', correctIndex: 1, answerImageO: '/assets/quiz-3-o.png', answerImageX: '/assets/quiz-3-x.png' },
   { id: 4, image: '/assets/quiz-4.png', correctIndex: 3, answerImageO: '/assets/quiz-4-o.png', answerImageX: '/assets/quiz-4-x.png' },
   { id: 5, image: '/assets/quiz-5.png', correctIndex: 2, answerImageO: '/assets/quiz-5-o.png', answerImageX: '/assets/quiz-5-x.png' },
+  { id: 6, image: '/assets/quiz-5.png', correctIndex: 2, answerImageO: '/assets/quiz-5-o.png', answerImageX: '/assets/quiz-5-x.png' }
 ]
